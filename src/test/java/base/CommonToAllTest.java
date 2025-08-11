d24d34ce0bf5c2f4c83f8645713493efe73340ca
sushant.narute <sushant.narute@akronsystems.com>
2025-08-11 17:36:14 +0530

    framwork a1

---
+++ src/test/java/base/CommonToAllTest.java
@@ -0,0 +1,23 @@
+package base;
+
+
+
+import Drivers.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+
+
+
+public class CommonToAllTest {
+    @BeforeMethod
+    public void Setup() {
+        DriverManager.init();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.quit();
+    }
+}
